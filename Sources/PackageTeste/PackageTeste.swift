98f02f6e1a06862eafb8b8a56aaad13bd7d96319
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-11-07 17:36:19 -0300

    Initial Commit

--- Sources/PackageTeste/PackageTeste.swift
+++ Sources/PackageTeste/PackageTeste.swift
@@ -9,4 +9,16 @@ extension View{
         }
     }
 }
+
+struct GrowingButton: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(.blue)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
  
